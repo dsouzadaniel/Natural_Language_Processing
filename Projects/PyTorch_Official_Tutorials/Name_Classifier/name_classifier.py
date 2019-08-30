1ff5b4651ab3f3e96eecdb498d2b031e6a207462
danieldsouza <ddsouza@umich.edu>
2019-08-30 18:44:03 -0400

    # Checked Embedding code

--- Projects/PyTorch_Official_Tutorials/Name_Classifier/name_classifier.py
+++ Projects/PyTorch_Official_Tutorials/Name_Classifier/name_classifier.py
@@ -4,6 +4,7 @@ import glob
 
 import unicodedata
 import string
+import torch
 
 # Paths
 DATA_GLOB_PATH = os.path.join('data', 'names', '*.txt')
@@ -21,19 +22,40 @@ def unicodeToAscii(s):
     )
 
 
+# Helper Function to read data from some_filepath
 def readData(some_filepath):
     language = os.path.basename(some_filepath).split('.')[0]
     names = []
     with open(some_filepath, 'r', encoding='utf-8') as f:
-        names.extend([line.strip() for line in f.readlines()])
+        names.extend([unicodeToAscii(line.strip()) for line in f.readlines()])
     return language, names
+
+
+def char_2_ix(some_character):
+    return all_letters.find(some_character)
+
+
+def name_2_tensor(some_name):
+    name_tensor = torch.zeros(len(some_name), 1, vocab_size)
+    # print(name_tensor.size())
+    for ix, charac in enumerate(some_name):
+        name_tensor[ix][0][char_2_ix(charac)] = 1
+    return name_tensor
 
 
 # Reading the Data into a Dictionary
 data = {k: v for (k, v) in [readData(curr_file_path) for curr_file_path in glob.glob(DATA_GLOB_PATH)]}
 
 # # Print Some of the Data
-print(data.keys())
-print(data['Arabic'][:5])
-print(data['English'][:5])
-print(data['Chinese'][:5])
+# print(data.keys())
+# print(data['Arabic'][:5])
+# print(data['English'][:5])
+# print(data['Chinese'][:5])
+
+# example = 'Daniel'
+# for ix, char in enumerate(example):
+#     print(char, "->", char_2_ix(char))
+#
+# print(name_2_tensor('Daniel'))
+
+
