f5bf61cbb6a50d3c82796ebf0709492ddfdb42ea
danieldsouza <ddsouza@umich.edu>
2019-08-30 20:40:30 -0400

    # Tested RNN

--- Projects/PyTorch_Official_Tutorials/Name_Classifier/name_classifier.py
+++ Projects/PyTorch_Official_Tutorials/Name_Classifier/name_classifier.py
@@ -5,6 +5,8 @@ import glob
 import unicodedata
 import string
 import torch
+import torch.nn as nn
+
 
 # Paths
 DATA_GLOB_PATH = os.path.join('data', 'names', '*.txt')
@@ -45,6 +47,7 @@ def name_2_tensor(some_name):
 
 # Reading the Data into a Dictionary
 data = {k: v for (k, v) in [readData(curr_file_path) for curr_file_path in glob.glob(DATA_GLOB_PATH)]}
+categories = list(data.keys())
 
 # # Print Some of the Data
 # print(data.keys())
@@ -58,4 +61,34 @@ data = {k: v for (k, v) in [readData(curr_file_path) for curr_file_path in glob.
 #
 # print(name_2_tensor('Daniel'))
 
+###### Defining the Network ########
 
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.i2o = nn.Linear(input_size+hidden_size, output_size)
+        self.i2h = nn.Linear(input_size+hidden_size, hidden_size)
+
+        self.LogSoftmax = nn.LogSoftmax(dim=1)
+
+    def init_hidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+    def forward(self, char_embedding, hidden_state):
+        combined_state = torch.cat((char_embedding, hidden_state), dim=1)
+        next_hidden_state = self.i2h(combined_state)
+        output_state = self.i2o(combined_state)
+        output_state = self.LogSoftmax(output_state)
+        return output_state, next_hidden_state
+#
+# sample_input = name_2_tensor('Daniel')
+# our_rnn = RNN(input_size=vocab_size, hidden_size=128, output_size=len(categories))
+# init_hidden = our_rnn.init_hidden()
+#
+# curr_out, curr_hidden = our_rnn(sample_input[0], init_hidden)
+#
+# print("Current Output is {0}".format(curr_out))
+# print("Current Hidden is {0}".format(curr_hidden))
