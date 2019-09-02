9912ebbbe4355354ae42b9f4c3adc8d449edb1ba
danieldsouza <ddsouza@umich.edu>
2019-09-02 12:19:30 -0400

    # Training Run Verified

--- Projects/PyTorch_Official_Tutorials/Name_Classifier/name_classifier.py
+++ Projects/PyTorch_Official_Tutorials/Name_Classifier/name_classifier.py
@@ -7,6 +7,8 @@ import string
 import random
 import torch
 import torch.nn as nn
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 # Paths
 DATA_GLOB_PATH = os.path.join('data', 'names', '*.txt')
@@ -49,14 +51,15 @@ def name_2_tensor(some_name):
 data = {k: v for (k, v) in [readData(curr_file_path) for curr_file_path in glob.glob(DATA_GLOB_PATH)]}
 categories = list(data.keys())
 
-data_tuples = [(name,ctgry) for ctgry in categories for name in data[ctgry]]
+data_tuples = [(name, ctgry) for ctgry in categories for name in data[ctgry]]
 random.shuffle(data_tuples)
 
 training_tuples = data_tuples[:15000]
 validation_tuples = data_tuples[15000:17500]
 test_tuples = data_tuples[17500:]
 
-print("Dataset Sizes are : Training({0}), Validation({1}), Test({2}) ".format(len(training_tuples), len(validation_tuples), len(test_tuples)))
+print("Dataset Sizes are : Training({0}), Validation({1}), Test({2}) ".format(len(training_tuples),
+                                                                              len(validation_tuples), len(test_tuples)))
 
 
 # # Print Some of the Data
@@ -113,7 +116,88 @@ def get_random_data_pair():
     return random_name, random_name_tensor, random_category, random_category_tensor
 
 
-
 # for i in range(10):
 #     curr_name, curr_name_tensor, curr_category, curr_category_tensor = get_random_data_pair()
 #     print("Name : {0} \t Category : {1}".format(curr_name, curr_category))
+
+
+char_rnn = RNN(input_size=vocab_size, hidden_size=128, output_size=len(categories))
+
+learning_rate = 0.005
+
+
+def train(data_tuple):
+    curr_name, curr_ctgry = data_tuple
+    curr_name_embedding = name_2_tensor(curr_name)
+
+    curr_hidden = char_rnn.init_hidden()
+
+    char_rnn.zero_grad()
+    for curr_char_embedding in curr_name_embedding:
+        curr_output, curr_hidden = char_rnn(curr_char_embedding, curr_hidden)
+    curr_target = torch.tensor([categories.index(curr_ctgry)], dtype=torch.long)
+
+    loss_func = nn.NLLLoss()
+
+    curr_loss = loss_func(curr_output, curr_target)
+    curr_loss.backward()
+    #
+    #
+    for each_param in char_rnn.parameters():
+        each_param.data.add_(-learning_rate, each_param.grad.data)
+
+
+    return curr_loss.item()
+
+
+def eval(data_tuple):
+    curr_name, curr_ctgry = data_tuple
+    curr_name_embedding = name_2_tensor(curr_name)
+
+    curr_hidden = char_rnn.init_hidden()
+
+    char_rnn.zero_grad()
+    for curr_char_embedding in curr_name_embedding:
+        curr_output, curr_hidden = char_rnn(curr_char_embedding, curr_hidden)
+    curr_target = torch.tensor([categories.index(curr_ctgry)], dtype=torch.long)
+
+    loss_func = nn.NLLLoss()
+
+    curr_loss = loss_func(curr_output, curr_target)
+
+    return curr_loss.item()
+
+
+train(('Giannis', 'Greek'))
+
+NUM_EPOCHS = 50
+
+
+collected_train_loss = []
+collected_valid_loss = []
+
+
+for epoch in range(NUM_EPOCHS):
+    epoch_train_loss = 0
+    epoch_valid_loss = 0
+    for single_training_tuple in training_tuples:
+        curr_train_loss = train(single_training_tuple)
+        epoch_train_loss+=curr_train_loss
+
+    with torch.no_grad():
+        for single_valid_tuple in validation_tuples:
+            curr_valid_loss = eval(single_valid_tuple)
+            epoch_valid_loss+=curr_valid_loss
+
+    epoch_train_loss = epoch_train_loss/len(training_tuples)
+    epoch_valid_loss = epoch_valid_loss/len(validation_tuples)
+
+    collected_train_loss.append(epoch_train_loss)
+    collected_valid_loss.append(epoch_valid_loss)
+    print("Epoch {0} \t Training Loss : {1} \t Valid Loss : {2}".format(epoch, epoch_train_loss, epoch_valid_loss))
+
+
+plt.figure()
+plt.plot(collected_train_loss)
+plt.plot(collected_valid_loss)
+plt.savefig('loss_graph.png')
