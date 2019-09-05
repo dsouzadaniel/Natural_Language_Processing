94580678a3f3df606d92beb9a42eef887c92fea0
danieldsouza <ddsouza@umich.edu>
2019-09-05 18:43:05 -0400

    # gtg on code/plots

--- Projects/PyTorch_Official_Tutorials/Name_Classifier/name_classifier.py
+++ Projects/PyTorch_Official_Tutorials/Name_Classifier/name_classifier.py
@@ -9,6 +9,7 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+from random import shuffle
 
 # Paths
 DATA_GLOB_PATH = os.path.join('data', 'names', '*.txt')
@@ -146,7 +147,6 @@ def train(data_tuple):
     for each_param in char_rnn.parameters():
         each_param.data.add_(-learning_rate, each_param.grad.data)
 
-
     return curr_loss.item()
 
 
@@ -170,34 +170,66 @@ def eval(data_tuple):
 
 train(('Giannis', 'Greek'))
 
-NUM_EPOCHS = 50
-
+NUM_EPOCHS = 30
 
 collected_train_loss = []
 collected_valid_loss = []
 
-
 for epoch in range(NUM_EPOCHS):
     epoch_train_loss = 0
     epoch_valid_loss = 0
+    shuffle(training_tuples)
     for single_training_tuple in training_tuples:
         curr_train_loss = train(single_training_tuple)
-        epoch_train_loss+=curr_train_loss
+        epoch_train_loss += curr_train_loss
 
     with torch.no_grad():
         for single_valid_tuple in validation_tuples:
             curr_valid_loss = eval(single_valid_tuple)
-            epoch_valid_loss+=curr_valid_loss
+            epoch_valid_loss += curr_valid_loss
 
-    epoch_train_loss = epoch_train_loss/len(training_tuples)
-    epoch_valid_loss = epoch_valid_loss/len(validation_tuples)
+    epoch_train_loss = epoch_train_loss / len(training_tuples)
+    epoch_valid_loss = epoch_valid_loss / len(validation_tuples)
 
     collected_train_loss.append(epoch_train_loss)
     collected_valid_loss.append(epoch_valid_loss)
     print("Epoch {0} \t Training Loss : {1} \t Valid Loss : {2}".format(epoch, epoch_train_loss, epoch_valid_loss))
 
-
 plt.figure()
 plt.plot(collected_train_loss)
 plt.plot(collected_valid_loss)
 plt.savefig('loss_graph.png')
+
+confusion = torch.zeros(len(categories), len(categories))
+
+def test_evaluate(curr_name):
+    curr_name_embedding = name_2_tensor(curr_name)
+
+    curr_hidden = char_rnn.init_hidden()
+
+    char_rnn.zero_grad()
+    for curr_char_embedding in curr_name_embedding:
+        curr_output, curr_hidden = char_rnn(curr_char_embedding, curr_hidden)
+
+    curr_pred_value, curr_pred_ix = curr_output.topk(1)
+    return curr_pred_ix
+
+
+with torch.no_grad():
+    for single_test_tuple in test_tuples:
+        curr_name, curr_ctgry = single_test_tuple
+        pred_ix = test_evaluate(curr_name)
+        confusion[categories.index(curr_ctgry), pred_ix] += 1
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(confusion.numpy())
+fig.colorbar(cax)
+
+ax.set_xticklabels([''] + categories, rotation=90)
+ax.set_yticklabels([''] + categories)
+
+ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+plt.savefig('test_confusion.png')
